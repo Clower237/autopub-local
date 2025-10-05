e918759799f32d0b06b1487a7391c50bc56a85b2
Clower237 <cloweramilly@gmail.com>
2025-10-05 17:39:26 +0200

    db: use postgres on render

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
-
+# database.py
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./autopub_local.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Render fournit un URL postgresql:// ; SQLAlchemy accepte aussi postgresql+psycopg
+if DATABASE_URL:
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+else:
+    engine = create_engine("sqlite:///./autopub_local.db", connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
